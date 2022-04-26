9e3e01557cea04f4938f0b83ac0041306be54bbc
ShehanSumudhitha <76446800+ShehanSumudhitha@users.noreply.github.com>
2022-04-26 17:03:51 +0530

    first commit

---
+++ planning.js
@@ -0,0 +1,24 @@
+//Book my Show planning
+
+// ->We need a reuseable carousel
+// ->Main/Hero carousel
+// ->common carousel
+// -> Cast and crew small carousel
+
+//Pages 
+// ->Home Page 
+// -> Movie page
+//-> Categories Page
+// -> Authentication Page
+// -> RazorPay payment gateway
+
+//Plan -> Howare we building it?
+//Layouts
+//Routes
+//Pages
+//Compnents
+
+//HOC
+//Higher Order Components
+
+
